6287f5c638966dc5804aaf7f657713d318e8b6d4
mrome007 <mrome007@ucr.edu>
2018-01-10 21:51:25 -0800

    Add basic enemy and interaction with world object. TODO create wave system

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy : MonoBehaviour 
+{
+    public event EventHandler EnemySpawned;
+    public event EventHandler EnemyDeath;
+
+    public virtual void SpawnEnemy()
+    {
+        var handler = EnemySpawned;
+        if(handler != null)
+        {
+            handler(this, null);
+        }
+    }
+
+    public virtual void KillEnemy()
+    {
+        var handler = EnemyDeath;
+        if(handler != null)
+        {
+            handler(this, null);
+        }
+    }
+}
---
+++ Assets/Scripts/KamikazeEnemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KamikazeEnemy : Enemy
+{
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float delayBeforeAttack;
+
+    private void Start()
+    {
+        SpawnEnemy();
+    }
+
+    private void Update()
+    {
+        if(delayBeforeAttack > 0f)
+        {
+            delayBeforeAttack -= Time.deltaTime;
+            return;
+        }
+
+        transform.Translate(transform.forward * speed * Time.deltaTime);
+    }
+
+    private void OnDestroy()
+    {
+        KillEnemy();
+    }
+}
